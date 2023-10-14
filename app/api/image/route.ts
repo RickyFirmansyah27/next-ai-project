714a0ce164302edb6cf77187f8fa60ecf63ee498
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-10-14 16:56:12 +0700

    enhance image ai

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,15 +1,14 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { Configuration, OpenAIApi } from "openai";
 
 import { checkSubscription } from "@/lib/subscription";
 import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
 
-const configuration = new Configuration({
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-const openai = new OpenAIApi(configuration);
 
 export async function POST(
   req: Request
@@ -17,26 +16,18 @@ export async function POST(
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { prompt, amount = 1, resolution = "512x512" } = body;
+    const { messages  } = body;
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    if (!configuration.apiKey) {
+    if (!openai.apiKey) {
       return new NextResponse("OpenAI API Key not configured.", { status: 500 });
     }
 
-    if (!prompt) {
-      return new NextResponse("Prompt is required", { status: 400 });
-    }
-
-    if (!amount) {
-      return new NextResponse("Amount is required", { status: 400 });
-    }
-
-    if (!resolution) {
-      return new NextResponse("Resolution is required", { status: 400 });
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
     }
 
     const freeTrial = await checkApiLimit();
@@ -46,19 +37,18 @@ export async function POST(
       return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
     }
 
-    const response = await openai.createImage({
-      prompt,
-      n: parseInt(amount, 10),
-      size: resolution,
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages
     });
 
     if (!isPro) {
       await incrementApiLimit();
     }
 
-    return NextResponse.json(response.data.data);
+    return NextResponse.json(response.choices);
   } catch (error) {
-    console.log('[IMAGE_ERROR]', error);
+    console.log('[CONVERSATION_ERROR]', error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 };
